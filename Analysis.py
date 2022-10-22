eba7dc63ddc6205e29eccee03dea0ca21e34598a
Admin <gesin.id@edu.spbstu.ru>
2022-10-22 13:20:11 +0300

    Initial commit

---
+++ Analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy import stats
+from sklearn.metrics import r2_score
+
+Anchoice = input(' If you want to analise Emission enter Em, for Defects on one step enter def1, for defects on multiple steps enter')
